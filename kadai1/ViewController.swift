00c7644dbf573aded86a1fbc2b9743c2d98c8b77
akio0911 <akio0911@gmail.com>
2024-03-04 13:41:44 +0900

    feedback

--- kadai1/ViewController.swift
+++ kadai1/ViewController.swift
@@ -8,34 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    //    入力フォーム定義
+
+    // 入力フォーム定義
     @IBOutlet weak var value1: UITextField!
     @IBOutlet weak var value2: UITextField!
     @IBOutlet weak var value3: UITextField!
     @IBOutlet weak var value4: UITextField!
     @IBOutlet weak var value5: UITextField!
-    
+
     @IBOutlet weak var result: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    //    和算処理
+    // 和算処理
     @IBAction func sumValue(_ sender: Any) {
-//        string->intへ変換
+        // String->Intへ変換
         let num1 = Int(value1.text ?? "") ?? 0
         let num2 = Int(value2.text ?? "") ?? 0
         let num3 = Int(value3.text ?? "") ?? 0
         let num4 = Int(value4.text ?? "") ?? 0
         let num5 = Int(value5.text ?? "") ?? 0
-            
+
         let sumResult = num1 + num2 + num3 + num4 + num5
-                
+
         result.text = String(sumResult)
-        
     }
-    
 }
